2f7699c5581216da54f3b4c661eed5879d8e89ef
Gabriel Camurça <projetosdocamurca@gmail.com>
2024-11-30 09:50:28 -0300

    organize code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-from database import database, SessionLocal
+from database.database import database, SessionLocal
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
